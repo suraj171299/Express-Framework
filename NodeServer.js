f78f0a57b4742c0a96fc252137a88389010179db
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-04 12:05:00 +0530

    Routing(express-assign-3)

--- NodeServer.js
+++ NodeServer.js
@@ -2,22 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-    res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>'
-    );
-});
+app.use("/admin", adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-    res.send("<h1>Hello from Express</h1>");
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 app.listen(3000);
